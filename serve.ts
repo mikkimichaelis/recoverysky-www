9000952ffeb9129391ea8bad8cf85e96df71cf44
Mikki Michaelis <mikkimichaelis@gmail.com>
2024-10-14 18:26:38 -0500

    default *.html

--- serve.ts
+++ serve.ts
@@ -4,7 +4,7 @@ const www = require('path').join(__dirname, 'www');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(www));
+app.use(express.static(www, { extensions: ['html'] }));
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
