656746baf2f6cbb79d3b2b42aaf04e5a56a62e21
Mikki Michaelis <mikkimichaelis@gmail.com>
2024-10-14 18:33:08 -0500

    add helmet

--- serve.ts
+++ serve.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import helmet from 'helmet';
 const www = require('path').join(__dirname, 'www');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(helmet());
 app.use(express.static(www, { extensions: ['html'] }));
 
 app.listen(port, () => {
